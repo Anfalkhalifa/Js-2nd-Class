74d9fd925b96dd9ed66c06e77855981809fd2f88
anfal khalifa <anfi.memon@gmail.com>
2017-03-10 01:49:45 +0500

    Js 2nd Class

---
+++ app.js
@@ -0,0 +1,18 @@
+var a = prompt("Enter Your Percentage", "Enter Value in Numbers");
+
+
+if (a > 70) {
+    document.write("Congratulation You Have <b>A</b> Grade")
+}
+else if (a > 60) {
+    document.write("Congragulation You Have <b>B</b> Grade")
+
+} else {
+    document.write("You Have <b>C</b> Grade")
+};
+
+    
+
+
+
+
